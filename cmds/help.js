1898a07aa60d02d244d3c9a2600593616fcc086f
Ginanjar <egin066@gmail.com>
2018-05-18 07:40:26 +0700

    add tempmute

--- cmds/help.js
+++ cmds/help.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("**Hai! I'm IceTea.** :tropical_drink: ")
     .setDescription("Command List")
     .addField("IceTea", "`help, ping, botinfo, avatar, serverinfo, serverroles, userinfo`")
-    .addField("Moderation", "`Cooming soon...`")
+    .addField("Moderation", "`tempmute`")
     .addField("Info Command", "ice?<command> help")
     .addField("Link", "[Invite](https://discordapp.com/api/oauth2/authorize?client_id=446436555985256448&permissions=1391470694&scope=bot)")
     .setTimestamp(new Date());
---
+++ cmds/tempmute.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+    /**
+     * command
+     * ice?tempmute @user time(s/m/h)
+     * 
+     * egin10
+     */
+    let info = new Discord.RichEmbed()
+        .setTitle("Usage **tempmute**")
+        .setColor("RANDOM")
+        .addField("Command", "ice?tempmute @member time")
+        .addField("Tempmute", "Mute member based on the time specified.");
+    if(args[0] == "help") return message.channel.send(info);
+
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!tomute) return message.reply("User not found!.");
+    if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have permission!");
+    let muterole = message.guild.roles.find(`name`, "muted");
+
+    //start of create role
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "muted",
+          color: "#a81d05",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      }catch(e){
+        console.log(e.stack);
+      }
+      
+    }
+
+    //end of create role
+    let mutetime = args[1];
+    if(!mutetime) return message.reply("Fill a correct time!");
+
+    await(tomute.addRole(muterole.id));
+    message.reply(`<@${tomute.id}> is **mute** for ${ms(ms(mutetime))}`);
+
+    setTimeout(function(){
+      tomute.removeRole(muterole.id);
+      message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    }, ms(mutetime));
+
+}
+
+module.exports.help = {
+  name: "tempmute"
+}
